3f96795ed9948917304b2121d91f528b8eae96b9
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-07 19:00:25 +0200

    main

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
-import Spinner from '../spinner/Spinner';import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage'
+import MarvelService from '../../services/MarvelService';
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
@@ -11,12 +13,23 @@ class RandomChar extends Component{
     }
     state = {
         char:{},
-        loading: true
+        loading: true,
+        error: false
+
     }
     marvelService = new MarvelService();
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({
+            char, 
+            loading: false})
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error:true
+        })
     }
 
     updateChar = () => {
@@ -24,14 +37,20 @@ class RandomChar extends Component{
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError);
     }
 
     render () {
-        const {char, loading} = this.state
+        const {char, loading, error} = this.state
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char = {char} /> : null;
 
         return (
             <div className="randomchar">
-                {loading ? <Spinner/> : <View char = {char} />}
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
