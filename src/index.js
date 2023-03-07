da25094d67d2753a32976b431da89c1e6e50f303
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-07 16:53:07 +0200

    main

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MarvelService from './services/MarvelService';
+import App from './components/app/App';
+
+import './style/style.scss';
+
+const marvelService = new MarvelService();
+marvelService.getAllCharacters().then(res => console.log(res))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App/>
   </React.StrictMode>
 );
 
