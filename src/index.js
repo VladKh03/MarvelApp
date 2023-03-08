7cfeb392a5a6fe0408569b770de5bd7af7429101
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-08 10:10:14 +0200

    main

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/app/App';
 import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App/>
