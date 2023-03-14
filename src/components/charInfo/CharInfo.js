cfeda6c89cfe66e35be43161c93cbcf3f5f8aafd
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-14 17:23:12 +0200

    main

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link, useParams } from 'react-router-dom';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -31,7 +32,7 @@ const CharInfo = (props) => {
     const onCharLoaded = (char) => {
         setChar(char);
     }
-    
+        
     const skeleton = char || loading || error ? null : <Skeleton/>;
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
@@ -49,11 +50,11 @@ const CharInfo = (props) => {
 
 const View = ({char}) => {
     const {name, description, thumbnail, homepage, wiki, comics} = char;
-
     let imgStyle = {'objectFit' : 'cover'};
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
+
     return (
         <>
             <div className="char__basics">
@@ -76,14 +77,19 @@ const View = ({char}) => {
             <div className="char__comics">Comics:</div>
             <ul className="char__comics-list">
                 {comics.length > 0 ? null : 'There is no comics with this character'}
-                {
+                {                    
                     comics.map((item, i) => {
                         // eslint-disable-next-line
                         if (i > 9) return;
                         return (
-                            <li key={i} className="char__comics-item">
-                                {item.name}
+                            <li key={i} className="char__comics-item">                                
+                                <Link to={`/comics/${
+                                    (item.resourceURI).slice(
+                                        item.resourceURI.length-5, 
+                                        item.resourceURI.length)
+                                        }`}>{item.name}</Link>
                             </li>
+                            
                         )
                     })
                 }                
