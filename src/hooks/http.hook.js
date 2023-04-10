eaefc4b566631a3ce93edabe7309c7a63735dcd8
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-04-10 17:46:22 +0300

    main

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -3,7 +3,6 @@ import { useState, useCallback } from "react";
 export const useHttp = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-
     const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
         setLoading(true)
 
